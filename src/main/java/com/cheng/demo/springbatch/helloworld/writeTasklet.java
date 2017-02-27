0356c22c2f24404eb7c17e94e7e13d4b89e11074
chengchenrui <380101538@qq.com>
2017-02-27 14:52:23 +0800

    spring-batch hello world

---
+++ src/main/java/com/cheng/demo/springbatch/helloworld/writeTasklet.java
@@ -0,0 +1,29 @@
+/**
+ * Bestpay.com.cn Inc.
+ * Copyright (c) 2011-2017 All Rights Reserved.
+ */
+package com.cheng.demo.springbatch.helloworld;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+/**
+ * @author chengchenrui
+ * @version Id: writeTasklet.java, v 0.1 2017.2.27 14:22 chengchenrui Exp $$
+ */
+public class writeTasklet implements Tasklet {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public RepeatStatus execute(StepContribution stepContribution,
+                                ChunkContext chunkContext) throws Exception {
+        System.out.println(message);
+        return RepeatStatus.FINISHED;
+    }
+}
