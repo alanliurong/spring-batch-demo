bdf150ebce82726b2b018a824fa040fed7849473
chengchenrui <380101538@qq.com>
2017-03-08 15:03:33 +0800

    添加过程

---
+++ spring-batch-jdbc/src/main/java/com/cheng/springbatch/jdbc/JdbcProcessor.java
@@ -0,0 +1,26 @@
+/**
+ * Bestpay.com.cn Inc.
+ * Copyright (c) 2011-2017 All Rights Reserved.
+ */
+package com.cheng.springbatch.jdbc;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author chengchenrui
+ * @version Id: JdbcProcessor.java, v 0.1 2017.3.8 15:00 chengchenrui Exp $$
+ */
+@Component("jdbcProcessor")
+public class JdbcProcessor implements ItemProcessor<User,User>{
+
+    public User process(User item) throws Exception {
+
+        item.setUpdateTime(new Date());
+        item.setUpdateUser("ADMIN");
+
+        return item;
+    }
+}
